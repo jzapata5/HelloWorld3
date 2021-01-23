1d6a6cec364a507062741c565b62ac24acb20656
jzapata5 <70824305+jzapata5@users.noreply.github.com>
2021-01-22 19:18:41 -0500

    Hello World 1st Project finished

--- app/src/main/java/com/example/helloworld3/MainActivity.java
+++ app/src/main/java/com/example/helloworld3/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.helloworld3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initDisplayButton();
+    }
+
+    private void initDisplayButton() {
+        Button displayButton = findViewById(R.id.buttonDisplay);
+        displayButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText editName = findViewById(R.id.editTextName);
+                TextView textDisplay = findViewById(R.id.textViewDisplay);
+                String nameToDisplay = editName.getText().toString();
+
+                Resources res = getResources(); // Asks the system for access to the values files
+                String displayString = res.getString(R.string.welcome_message, nameToDisplay); // Creates the string with a message and name
+                textDisplay.setText(displayString);
+            }
+        });
+
+
+
     }
 }
